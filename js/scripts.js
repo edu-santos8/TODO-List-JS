b8dd5180236cda4353c8e3d4682bb80c267de5e2
Eduardo Santos <122983424+edu-santos8@users.noreply.github.com>
2023-02-10 11:14:56 +0000

    JS Functions
    Adding the necessary Javascript functions that connects to the classes in the html to this project before linking to the js events.

--- js/scripts.js
+++ js/scripts.js
@@ -6,18 +6,45 @@ const editForm = document.querySelector("#edit-form");
 const editInput = document.querySelector("#edit-input");
 const cancelEditBtn = document.querySelector("#cancel-edit-btn");
 
-
 // Functions
+const saveTodo = (text) => {
 
+    const todo = document.createElement("div");
+    todo.classList.add("todo");
+
+    const todoTitle = document.createElement("h3");
+    todoTitle.innerText = text;
+    todo.appendChild(todoTitle);
+
+    const doneBtn = document.createElement("button");
+    doneBtn.classList.add("finish-todo");
+    doneBtn.innerHTML = '<p class="emoji">&#9989</p>';
+    todo.appendChild(doneBtn);
+
+    const editBtn = document.createElement("button");
+    editBtn.classList.add("edit-todo");
+    editBtn.innerHTML = '<p class="emoji">&#128394</p>';
+    todo.appendChild(editBtn);
+
+    const deleteBtn = document.createElement("button");
+    deleteBtn.classList.add("remove-todo");
+    deleteBtn.innerHTML = '<p class="emoji">&#10060</p>';
+    todo.appendChild(deleteBtn);
+
+    todoList.appendChild(todo);
+
+    todoInput.value = "";
+    todoInput.focus();
+
+}
 
 // Eventos
 todoForm.addEventListener("submit", (e) => {
     e.preventDefault();
 
-    const inputValue = todoInput.value
+    const inputValue = todoInput.value;
 
     if (inputValue) {
-        console.log(inputValue);
-        // save todo
+        saveTodo(inputValue);
     }
 });
