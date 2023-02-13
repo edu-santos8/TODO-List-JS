86b30ebec555cef70f16b91d422b6c950e7ab4ee
Eduardo Santos <122983424+edu-santos8@users.noreply.github.com>
2023-02-13 11:49:12 +0000

    ToDo finalized
    Finalized the webapp to-do list with options to mark tasks already done, edit and delete options working properly.

--- js/scripts.js
+++ js/scripts.js
@@ -6,6 +6,8 @@ const editForm = document.querySelector("#edit-form");
 const editInput = document.querySelector("#edit-input");
 const cancelEditBtn = document.querySelector("#cancel-edit-btn");
 
+let oldInputValue;
+
 // Functions
 const saveTodo = (text) => {
 
@@ -44,6 +46,20 @@ const toggleForms = () => {
     todoList.classList.toggle("hide");
 };
 
+const updateTodo = (text) => {
+
+    const todos = document.querySelectorAll(".todo");
+
+    todos.forEach((todo) => {
+
+        let todoTitle = todo.querySelector("h3");
+
+        if (todoTitle.innerText === oldInputValue) {
+            todoTitle.innerText = text;
+        }
+    });
+};
+
 // Eventos
 todoForm.addEventListener("submit", (e) => {
     e.preventDefault();
@@ -58,6 +74,11 @@ todoForm.addEventListener("submit", (e) => {
 document.addEventListener("click", (e) => {
     const targetEl = e.target;
     const parentEl = targetEl.closest("div");
+    let todoTitle;
+    
+    if (parentEl && parentEl.querySelector("h3")) {
+        todoTitle = parentEl.querySelector("h3").innerText;
+    }
 
     if (targetEl.classList.contains("finish-todo")) {
         parentEl.classList.toggle("done");
@@ -69,6 +90,9 @@ document.addEventListener("click", (e) => {
 
     if (targetEl.classList.contains("edit-todo")) {
         toggleForms();
+
+        editInput.value = todoTitle;
+        oldInputValue = todoTitle;
     }
 });
 
@@ -77,3 +101,17 @@ cancelEditBtn.addEventListener("click", (e) => {
 
     toggleForms();
 });
+
+editForm.addEventListener("submit", (e) => {
+
+    e.preventDefault();
+
+    const editInputValue = editInput.value;
+
+    if(editInputValue) {
+        updateTodo(editInputValue);
+    }
+
+    toggleForms();
+
+})
